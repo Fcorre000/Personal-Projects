4ea74d02f842ea2cf8aa2773f5e96ffa634429e7
Fernando Correa <fernandocorrea2014@gmail.com>
2024-12-17 17:20:58 -0600

    New game directories

--- Game/src/game.cpp
+++ Game/src/game.cpp
@@ -5,24 +5,45 @@ using namespace std;
 //to compile
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(600, 400), "SFML works!");
-    sf::RectangleShape shape;
-    shape.setFillColor(sf::Color::Cyan);
-    shape.setSize(sf::Vector2f(100, 100));
-    shape.setOutlineColor(sf::Color::Green);
-    shape.setOutlineThickness(2);
-    shape.setPosition(200, 100);
+    sf::RenderWindow window(sf::VideoMode(600, 400), "Fernando's SFML Project!");
+    
 
     bool mouseWasReleased = false;
     bool AKeyWasReleased = false;
+    float xPosition = 0.0f;
 
+    //object for timing/fps
+    sf::Clock clock; 
+    window.setFramerateLimit(60);
+
+    //Texture object used to put sprite in
     sf::Texture texture;
-    texture.loadFromFile("/Users/fortnitekorea/Desktop/Personal-Projects/Game/maverick-sprite.jpeg");
+    texture.loadFromFile("/Users/fortnitekorea/Desktop/Personal-Projects/Game/assets/images/PlayerSprite.png");
     //setup our sprite with a texture
     sf::Sprite sprite(texture);
 
+    sprite.setScale(.3, .3);
+    
+
+    //Load a font, and setup a test 'test' for the program
+    sf::Font font;
+    font.loadFromFile("/Users/fortnitekorea/Desktop/Personal-Projects/Game/assets/fonts/Simple Heavy.otf");
+    sf::Text text("Bird From FORTNITE!!", font, 50);
+    text.setPosition(50, 50);//position set to see text on on background, isn't necessary
+    //this only confirms the text overlaps the sprite
+
+    //set font color to whatever, but for now red
+    text.setFillColor(sf::Color::Red);
+
     while (window.isOpen())
     {
+
+        //compute frame time
+        float currentTime = clock.restart().asSeconds();//returns time object and stored as a float
+        float fps = 1.0f/(currentTime);
+        //cout << "fps: " << fps << "\n";
+
+
         //event object created
         sf::Event event;
 
@@ -75,12 +96,20 @@ int main()
             AKeyWasReleased = false;
         }
 
+        sprite.setPosition(xPosition, 0.0f);
+        xPosition+= 1;
+        if(xPosition > 100){
+            xPosition = 0;
+        }
+
         //will continually clear, draw and display window
         //until program is closed.
         window.clear();
 
         //draw sprites
         window.draw(sprite);
+        //Draw our test text
+        window.draw(text);
         //window.draw(shape);
         window.display();
     }
